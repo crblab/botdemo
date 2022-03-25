b9f730081468a84ef4b4ef4d51d75d217ade17af
Hieu Pham <hieupt.ai@gmail.com>
2022-03-25 17:00:36 +0700

    Deploy action

--- next.config.js
+++ next.config.js
@@ -1,12 +1,32 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+  assetPrefix: isProd ? '/botdemo/' : '',
   reactStrictMode: true,
-  images:{
+  images: {
+    disableStaticImages: true,
     domains:[
       'links.papareact.com',
       'platform-lookaside.fbsbx.com',
       'firebasestorage.googleapis.com'
     ]
   },
-}
+};
 
+const config = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        // optimisation disabled by default, to enable check https://github.com/cyrilwanner/next-optimized-images
+        optimizeImages: false,
+      },
+    ],
+  ],
+  nextConfig
+);
 
+module.exports = config;
