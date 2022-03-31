7b72d253002cdf063ceff32978e0a52f15841378
klxcoder <klxcoder@gmail.com>
2022-03-31 21:42:37 +0700

    fix next.config.js config error

--- next.config.js
+++ next.config.js
@@ -1,29 +1,9 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
     disableStaticImages: true,
-    domains:[
-      'links.papareact.com',
-      'platform-lookaside.fbsbx.com',
-      'firebasestorage.googleapis.com'
-    ]
+    domains:[]
   },
 };
 
-const config = withPlugins(
-  [
-    [
-      optimizedImages,
-      {
-        // optimisation disabled by default, to enable check https://github.com/cyrilwanner/next-optimized-images
-        optimizeImages: false,
-      },
-    ],
-  ],
-  nextConfig
-);
-
-module.exports = config;
+module.exports = nextConfig;
